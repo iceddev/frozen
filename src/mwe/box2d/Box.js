8a3b86c5dbfc523e75f9ac6a5f21988a8ccea98a
Luis Montes <monteslu@gmail.com>
2012-02-10 09:31:30 -0700

    fix remove body and formatting

--- src/mwe/box2d/Box.js
+++ src/mwe/box2d/Box.js
@@ -42,122 +42,126 @@ var   b2Vec2 = Box2D.Common.Math.b2Vec2
 
 
 dojo.declare("mwe.box2d.Box",null,{
-	intervalRate : 60,
-	adaptive : false,
-	width : 640,
-	height : 480,
-	scale : 30,
-	bodiesMap : null,
-	world : null,
-	gravityX : 0,
-	gravityY: 10,
-	allowSleep: true,
+  intervalRate : 60,
+  adaptive : false,
+  width : 640,
+  height : 480,
+  scale : 30,
+  bodiesMap : null,
+  fixturesMap: null,
+  world : null,
+  gravityX : 0,
+  gravityY: 10,
+  allowSleep: true,
 
     constructor: function(args){
-		dojo.safeMixin(this, args);
-		if(args.intervalRate){
-			this.intervalRate = parseInt(args.intervalRate);
-		}
-		
-		if(!this.bodiesMap){
-			this.bodiesMap = [];
-		}
-		
-		this.world = new b2World(
-		        new b2Vec2(this.gravityX, this.gravityY)    
-		     ,  this.allowSleep                 
-		);
-		
+      dojo.safeMixin(this, args);
+      if(args.intervalRate){
+	this.intervalRate = parseInt(args.intervalRate);
+      }
+      
+      if(!this.bodiesMap){
+	this.bodiesMap = [];
+      }
+      if(!this.fixturesMap){
+	this.fixturesMap = [];
+      }
+      
+      this.world = new b2World(
+	      new b2Vec2(this.gravityX, this.gravityY)    
+	  ,  this.allowSleep                 
+      );
+    
     },
     
     update : function() {
-	  var start = Date.now();
-	  var stepRate = (this.adaptive) ? (now - this.lastTimestamp) / 1000 : (1 / this.intervalRate);
-	  this.world.Step(
-	         stepRate   //frame-rate
-	      ,  10       //velocity iterations
-	      ,  10       //position iterations
-	   );
-	   this.world.ClearForces();
-	   return (Date.now() - start);
-	},
-	
-	getState : function() {
-	  var state = {};
-	  for (var b = this.world.GetBodyList(); b; b = b.m_next) {
-	    if (b.IsActive() && typeof b.GetUserData() !== 'undefined' && b.GetUserData() != null) {
-	        state[b.GetUserData()] = {x: b.GetPosition().x, y: b.GetPosition().y, angle: b.GetAngle(), center: {x: b.GetWorldCenter().x, y: b.GetWorldCenter().y}};
-	    }
-	  }
-	  return state;
-	},
-	
-	setBodies : function(bodyEntities) {
-	    console.log('bodies',bodyEntities);
-	    for(var id in bodyEntities) {
-	        var entity = bodyEntities[id];
-	        this.addBody(entity);
-	    }
-	    this.ready = true;
-	},
-	
-	addBody : function(entity) {
-		var bodyDef = new b2BodyDef();
-	    var fixDef = new b2FixtureDef();
-	    fixDef.restitution = entity.restitution;
-	    fixDef.density = entity.density;
-	    fixDef.friction = entity.friction;
-	    
-	    
-	    if(entity.staticBody){
-	    	bodyDef.type =  b2Body.b2_staticBody;
-	    }else{
-	    	bodyDef.type = b2Body.b2_dynamicBody;
-	    }
-	    
-	    
-	    if (entity.radius) {
-	        fixDef.shape = new b2CircleShape(entity.radius);
-	    } else if (entity.points) {
-	        var points = [];
-	        for (var i = 0; i < entity.points.length; i++) {
-	            var vec = new b2Vec2();
-	            vec.Set(entity.points[i].x, entity.points[i].y);
-	            points[i] = vec;
-	        }
-	        fixDef.shape = new b2PolygonShape;
-	        fixDef.shape.SetAsArray(points, points.length);
-	    } else {
-	        fixDef.shape = new b2PolygonShape;
-	        fixDef.shape.SetAsBox(entity.halfWidth, entity.halfHeight);
-	    }
-	   bodyDef.position.x = entity.x;
-	   bodyDef.position.y = entity.y;
-	   bodyDef.userData = entity.id;
-	   bodyDef.linearDamping = entity.linearDamping;
-	   bodyDef.angularDamping = entity.angularDamping;
-	   this.bodiesMap[entity.id] = this.world.CreateBody(bodyDef);
-	   this.bodiesMap[entity.id].CreateFixture(fixDef);	    
-	    
-	},
-	
-	applyImpulse : function(bodyId, degrees, power) {
-		//console.log('bodiesMap',this.bodiesMap);
-	    var body = this.bodiesMap[bodyId];
-	    body.ApplyImpulse(new b2Vec2(Math.cos(degrees * (Math.PI / 180)) * power,
-	                                 Math.sin(degrees * (Math.PI / 180)) * power),
-	                                 body.GetWorldCenter());
-	    console.log('applying impluse',bodyId, degrees, power);
-	},
-	
-	removeBody : function(id) {
-		if(this.bodiesMap[id]){
-			this.world.DestroyBody(this.bodiesMap[id]);
-			
-		}
-	}
-	
+      var start = Date.now();
+      var stepRate = (this.adaptive) ? (now - this.lastTimestamp) / 1000 : (1 / this.intervalRate);
+      this.world.Step(
+	  stepRate   //frame-rate
+	,  10       //velocity iterations
+	,  10       //position iterations
+      );
+      this.world.ClearForces();
+      return (Date.now() - start);
+    },
+  
+  getState : function() {
+    var state = {};
+    for (var b = this.world.GetBodyList(); b; b = b.m_next) {
+      if (b.IsActive() && typeof b.GetUserData() !== 'undefined' && b.GetUserData() != null) {
+          state[b.GetUserData()] = {x: b.GetPosition().x, y: b.GetPosition().y, angle: b.GetAngle(), center: {x: b.GetWorldCenter().x, y: b.GetWorldCenter().y}};
+      }
+    }
+    return state;
+  },
+  
+  setBodies : function(bodyEntities) {
+      console.log('bodies',bodyEntities);
+      for(var id in bodyEntities) {
+          var entity = bodyEntities[id];
+          this.addBody(entity);
+      }
+      this.ready = true;
+  },
+  
+  addBody : function(entity) {
+    var bodyDef = new b2BodyDef();
+    var fixDef = new b2FixtureDef();
+    fixDef.restitution = entity.restitution;
+    fixDef.density = entity.density;
+    fixDef.friction = entity.friction;
     
+    
+    if(entity.staticBody){
+      bodyDef.type =  b2Body.b2_staticBody;
+    }else{
+      bodyDef.type = b2Body.b2_dynamicBody;
+    }
+    
+    
+    if (entity.radius) {
+	fixDef.shape = new b2CircleShape(entity.radius);
+    } else if (entity.points) {
+	var points = [];
+	for (var i = 0; i < entity.points.length; i++) {
+	    var vec = new b2Vec2();
+	    vec.Set(entity.points[i].x, entity.points[i].y);
+	    points[i] = vec;
+	}
+	fixDef.shape = new b2PolygonShape;
+	fixDef.shape.SetAsArray(points, points.length);
+    } else {
+	fixDef.shape = new b2PolygonShape;
+	fixDef.shape.SetAsBox(entity.halfWidth, entity.halfHeight);
+    }
+    bodyDef.position.x = entity.x;
+    bodyDef.position.y = entity.y;
+    bodyDef.userData = entity.id;
+    bodyDef.linearDamping = entity.linearDamping;
+    bodyDef.angularDamping = entity.angularDamping;
+    this.bodiesMap[entity.id] = this.world.CreateBody(bodyDef);
+    this.bodiesMap[entity.id].CreateFixture(fixDef);   
+      
+  },
+  
+  applyImpulse : function(bodyId, degrees, power) {
+    //console.log('bodiesMap',this.bodiesMap);
+    var body = this.bodiesMap[bodyId];
+    body.ApplyImpulse(new b2Vec2(Math.cos(degrees * (Math.PI / 180)) * power,
+				  Math.sin(degrees * (Math.PI / 180)) * power),
+				  body.GetWorldCenter());
+    //console.log('applying impluse',bodyId, degrees, power);
+  },
+  
+  removeBody : function(id) {
+    if(this.bodiesMap[id]){
+      this.bodiesMap[id].DestroyFixture(this.fixturesMap[id]);
+      this.world.DestroyBody(this.bodiesMap[id]);
+      delete this.fixturesMap[id];
+      delete this.bodiesMap[id];
+    }
+  }
     
     
 });
