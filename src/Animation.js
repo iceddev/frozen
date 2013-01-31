28e4057a996e6bf779d83f56da9c37cd910934ed
Blaine Bublitz <phate666x@gmail.com>
2013-01-31 01:14:48 -0700

    remove license from each file, added LICENSE file at base of project, add license to README, update license to Iced Development, LLC and 2013

--- src/Animation.js
+++ src/Animation.js
@@ -1,20 +1,8 @@
 /**
-
- Copyright 2011 Luis Montes
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-
-**/
+ * Represents a series of frames that can be rendered as an animation.
+ * @name Animation
+ * @class Animation
+ */
 
 define([
   './AnimFrame',
@@ -25,11 +13,6 @@ define([
 
   'use strict';
 
- /**
- * Represents a series of frames that can be rendered as an animation.
- * @name Animation
- * @class Animation
- */
 
   var Animation = dcl(Mixer, {
     currFrameIndex: 0,
--- src/GameCore.js
+++ src/GameCore.js
@@ -1,20 +1,21 @@
 /**
-
- Copyright 2011 Luis Montes
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-
-**/
+ * The GameCore class provides the base to build games on.
+ * @name GameCore
+ * @class GameCore
+ * @example
+ * var myGame = new GameCore({
+ *   canvasId: 'myCanvas',
+ *   update: function(millis){
+ *     // do updating of game state
+ *   },
+ *   draw: function(context){
+ *     // do drawing of the game
+ *   }
+ * });
+ *
+ * //start the game
+ * myGame.run();
+ */
 
 define([
   'dcl',
@@ -28,24 +29,6 @@ define([
 
   'use strict';
 
- /**
-  * The GameCore class provides the base to build games on.
-  * @name GameCore
-  * @class GameCore
-  * @example
-  * var myGame = new GameCore({
-  *   canvasId: 'myCanvas',
-  *   update: function(millis){
-  *     // do updating of game state
-  *   },
-  *   draw: function(context){
-  *     // do drawing of the game
-  *   }
-  * });
-  *
-  * //start the game
-  * myGame.run();
-  */
 
   return dcl(Mixer, {
     statics: {
