8f75683fbca4f873e5e890d2c2ce10cd5a38167c
Blaine Bublitz <blaine@iceddev.com>
2013-02-12 10:52:38 -0700

    cleanup scalePoints

--- src/utils/scalePoints.js
+++ src/utils/scalePoints.js
@@ -1,25 +1,28 @@
-define([
-  'dojo/_base/lang'
-], function(lang){
+define(function(lang){
 
   'use strict';
 
-  var scalePoints = function(points, scale){
-    if(lang.isArray(points)){
+  function scalePoints(points, scale){
+    if(Array.isArray(points)){
       var newPoints = [];
+      // TODO: move this to a faster forEach, possibly lo-dash's
       points.forEach(function(point){
         newPoints.push(scalePoints(point, scale));
       });
       points = newPoints;
-    }else{
-      if (typeof scale === 'object'){
-        points = {x: points.x * scale.x, y: points.y * scale.y};
-      }else{
-        points = {x: points.x * scale, y: points.y * scale};
-      }
+    } else if(typeof scale === 'object'){
+      points = {
+        x: points.x * scale.x,
+        y: points.y * scale.y
+      };
+    } else {
+      points = {
+        x: points.x * scale,
+        y: points.y * scale
+      };
     }
     return points;
-  };
+  }
 
   return scalePoints;
 
