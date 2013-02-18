65476c483eed03e7a68114f72e8c81e6268eba25
Blaine Bublitz <blaine@iceddev.com>
2013-02-18 13:49:28 -0700

    add docs to Sound plugin

--- src/sounds/Sound.js
+++ src/sounds/Sound.js
@@ -1,3 +1,16 @@
+/**
+ * AMD Plugin to load the correct Sound object
+ * @module sounds/Sound
+ * @example
+ * define([
+ *   'frozen/sounds/Sound!'
+ * ], function(Sound){
+ *
+ *   // Use the generic sound object API here - the correct Sound object will be used
+ *
+ * });
+ */
+
 define([
   'require',
   'dojo/has',
