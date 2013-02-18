be9994e0ba7d34bb45ddf8f75799c0e66a2722f5
Luis Montes <monteslu@gmail.com>
2013-02-18 15:12:11 -0700

    update box2d example

--- examples/box2d/game.js
+++ examples/box2d/game.js
@@ -1,26 +1,18 @@
 //load the AMD modules we need
-require(['frozen/GameCore', 'frozen/ResourceManager', 'dojo/keys', 'frozen/box2d/Box', 'frozen/box2d/RectangleEntity', 'frozen/box2d/PolygonEntity', 'frozen/box2d/CircleEntity'],
- function(GameCore, ResourceManager, keys, Box, Rectangle, Polygon, Circle){
+require([
+  'dojo/keys',
+  'frozen/box2d/BoxGame',
+  'frozen/box2d/RectangleEntity',
+  'frozen/box2d/PolygonEntity',
+  'frozen/box2d/CircleEntity',
+  'frozen/plugins/loadImage!images/background.png',
+  'frozen/plugins/loadImage!images/nyan.png',
+  'frozen/plugins/loadImage!images/yarn.png',
+  'frozen/plugins/loadSound!sounds/yipee.wav'
+  ],
+ function(keys, BoxGame, Rectangle, Polygon, Circle, backImg, nyanImg, yarnImg, yipee){
 
-  //dimensions same as canvas.
-  var gameH = 480;
-  var gameW = 770;
-  
   var speed = 8;
-
-  var output = document.getElementById('output');
-
-  var rm = new ResourceManager();
-  var backImg = rm.loadImage('images/background.png');
-  var nyanImg = rm.loadImage('images/nyan.png');
-  var yarnImg = rm.loadImage('images/yarn.png');
-  var yipee = rm.loadSound('sounds/yipee.wav');
-
-  var box;
-  var world = {};
-
-  //pixels per meter for box2d
-  var SCALE = 30.0;
 
   //objects in box2d need an id
   var geomId = 1;
@@ -30,187 +22,174 @@ require(['frozen/GameCore', 'frozen/ResourceManager', 'dojo/keys', 'frozen/box2d
 
 
 
-  // create our box2d instance
-  box = new Box({intervalRate:60, adaptive:false, width:gameW, height:gameH, scale:SCALE, gravityY:9.8, resolveCollisions: true,
-   postSolve: function(idA, idB, impulse){
-    if(impulse > 3){
-      console.log(idA, idB, impulse);
-    }
-   }});
-  
-
-  //create each of the shapes in the world
-  ground = new Rectangle({
-    id: geomId,
-    x: 385 / SCALE,
-    y: 480 / SCALE,
-    halfWidth: 1000 / SCALE,
-    halfHeight: 40 / SCALE,
-    staticBody: true
-  });
-  box.addBody(ground); //add the shape to the box
-  world[geomId] = ground; //keep a reference to the shape for fast lookup
-
-  celing = new Rectangle({
-    id: geomId,
-    x: 385 / SCALE,
-    y: -200 / SCALE,
-    halfWidth: 1000 / SCALE,
-    halfHeight: 40 / SCALE,
-    staticBody: true
-  });
-  box.addBody(celing);
-  world[geomId] = celing;
-
-  leftWall = new Rectangle({
-    id: geomId,
-    x: -80 / SCALE,
-    y: 240 / SCALE,
-    halfWidth: 40 / SCALE,
-    halfHeight: 1000 / SCALE,
-    staticBody: true
-  });
-  box.addBody(leftWall);
-  world[geomId] = leftWall;
-
-  rightWall = new Rectangle({
-    id: geomId,
-    x: 850 / SCALE,
-    y: 240 / SCALE,
-    halfWidth: 40 / SCALE,
-    halfHeight: 1000 / SCALE,
-    staticBody: true
-  });
-  box.addBody(rightWall);
-  world[geomId] = rightWall;
-
-  geomId++;
-  moon = new Circle({
-    id: geomId,
-    x: 626 / SCALE,
-    y: 120 / SCALE,
-    radius: 63 / SCALE,
-    staticBody: true
-  });
-  box.addBody(moon);
-  world[geomId] = moon;
-
-  geomId++;
-  pyramid = new Polygon({
-    id: geomId,
-    points: [{x: 320 / SCALE, y: 440 / SCALE}, {x: 446 / SCALE, y: 290 / SCALE}, {x: 565 / SCALE, y: 440 / SCALE}],
-    staticBody: true
-  });
-  box.addBody(pyramid);
-  world[geomId] = pyramid;
-
-  geomId++;
-  nyan = new Rectangle({
-    id: geomId,
-    x: 116 / SCALE,
-    y: 360 / SCALE,
-    halfWidth: 40 / SCALE,
-    halfHeight: 28 / SCALE,
-    staticBody: false,
-    draw: function(ctx, scale){ // we want to render the nyan cat with an image
-      ctx.save();
-      ctx.translate(this.x * scale, this.y * scale);
-      ctx.rotate(this.angle); // this angle was given to us by box2d's calculations
-      ctx.translate(-(this.x) * scale, -(this.y) * scale);
-      ctx.fillStyle = this.color;
-      ctx.drawImage(
-        nyanImg,
-        (this.x-this.halfWidth) * scale - 10, //lets offset it a little to the left
-        (this.y-this.halfHeight) * scale
-      );
-      ctx.restore();
-    }
-  });
-  box.addBody(nyan);
-  world[geomId] = nyan;
-
-  geomId++;
-  yarn = new Circle({
-    id: geomId,
-    x: 600 / SCALE,
-    y: 390 / SCALE,
-    radius: 30 / SCALE,
-    staticBody: false,
-    density: 0.5,  // al little lighter
-    restitution: 0.8, // a little bouncier
-    draw: function(ctx, scale){  //we also want to render the yarn with an image
-      ctx.save();
-      ctx.translate(this.x * scale, this.y * scale);
-      ctx.rotate(this.angle);
-      ctx.translate(-(this.x) * scale, -(this.y) * scale);
-      ctx.fillStyle = this.color;
-      ctx.drawImage(
-        yarnImg,
-        (this.x-this.radius) * scale,
-        (this.y-this.radius) * scale
-      );
-      ctx.restore();
-    }
-  });
-  box.addBody(yarn);
-  world[geomId] = yarn;
-
-
-
-
   //setup a GameCore instance
-  var game = new GameCore({
+  var game = new BoxGame({
     canvasId: 'canvas',
-    resourceManager: rm,
     initInput: function(im){
       //tells the input manager to listen for key events
       im.addKeyAction(keys.LEFT_ARROW);
       im.addKeyAction(keys.RIGHT_ARROW);
       im.addKeyAction(keys.UP_ARROW);
 
+      im.addKeyAction('A');
+      im.addKeyAction('D');
+
       //the extra param says to only detect inital press
       im.addKeyAction(keys.SPACE, true);
     },
     handleInput: function(im){
       if(im.keyActions[keys.LEFT_ARROW].isPressed()){
-        box.applyImpulseDegrees(nyan.id, 270, speed);
+        this.box.applyImpulseDegrees(nyan.id, 270, speed);
       }
 
       if(im.keyActions[keys.RIGHT_ARROW].isPressed()){
-        box.applyImpulseDegrees(nyan.id, 90, speed);
+        this.box.applyImpulseDegrees(nyan.id, 90, speed);
       }
 
       if(im.keyActions[keys.UP_ARROW].isPressed()){
-        box.applyImpulseDegrees(nyan.id, 0, speed);
+        this.box.applyImpulseDegrees(nyan.id, 0, speed);
+      }
+
+      if(im.keyActions['A'].isPressed()){
+        this.box.applyTorque(yarn.id, -speed);
+      }
+
+      if(im.keyActions['D'].isPressed()){
+        this.box.applyTorque(yarn.id, speed);
       }
 
       //.play sounds with the space bar !
       if(im.keyActions[keys.SPACE].getAmount()){
-        rm.playSound(yipee);
+        yipee.play();
       }
-
-      //when creating geometry, you may want to use the to determine where you are on the canvas
-      //if(im.mouseAction.position){
-        //output.innerHTML = 'x: ' + im.mouseAction.position.x + ' y: ' + im.mouseAction.position.y;
-      //}
-    },
-    update: function(millis){
-      
-      //have box2d do an interation
-      box.update();
-      //have update local objects with box2d calculations
-      box.updateExternalState(world);
 
     },
     draw: function(context){
       context.drawImage(backImg, 0, 0, this.width, this.height);
-      //ground.draw(context, SCALE);
-      //moon.draw(context, SCALE);
-      //pyramid.draw(context, SCALE);
-      nyan.draw(context, SCALE);
-      yarn.draw(context, SCALE);
+      nyan.draw(context);
+      yarn.draw(context);
     }
   });
+
+  //create each of the shapes in the world
+  ground = new Rectangle({
+    id: geomId,
+    x: 385,
+    y: 480,
+    halfWidth: 1000,
+    halfHeight: 40,
+    staticBody: true
+  });
+  game.box.addBody(ground); //add the shape to the box
+  game.entities[geomId] = ground; //keep a reference to the shape for fast lookup
+
+  geomId++;
+  celing = new Rectangle({
+    id: geomId,
+    x: 385,
+    y: -200,
+    halfWidth: 1000,
+    halfHeight: 40,
+    staticBody: true
+  });
+  game.box.addBody(celing);
+  game.entities[geomId] = celing;
+
+  geomId++;
+  leftWall = new Rectangle({
+    id: geomId,
+    x: -80,
+    y: 240,
+    halfWidth: 40,
+    halfHeight: 1000,
+    staticBody: true
+  });
+  game.box.addBody(leftWall);
+  game.entities[geomId] = leftWall;
+
+  geomId++;
+  rightWall = new Rectangle({
+    id: geomId,
+    x: 850,
+    y: 240,
+    halfWidth: 40,
+    halfHeight: 1000,
+    staticBody: true
+  });
+  game.box.addBody(rightWall);
+  game.entities[geomId] = rightWall;
+
+  geomId++;
+  moon = new Circle({
+    id: geomId,
+    x: 626,
+    y: 120,
+    radius: 63,
+    staticBody: true
+  });
+  game.box.addBody(moon);
+  game.entities[geomId] = moon;
+
+  geomId++;
+  pyramid = new Polygon({
+    id: geomId,
+    points: [{x: 320, y: 440}, {x: 446, y: 290}, {x: 565, y: 440}],
+    staticBody: true
+  });
+  game.box.addBody(pyramid);
+  game.entities[geomId] = pyramid;
+
+  geomId++;
+  nyan = new Rectangle({
+    id: 'nyan',
+    x: 116,
+    y: 360,
+    halfWidth: 40,
+    halfHeight: 28,
+    staticBody: false,
+    draw: function(ctx){ // we want to render the nyan cat with an image
+      ctx.save();
+      ctx.translate(this.x * this.scale, this.y * this.scale);
+      ctx.rotate(this.angle); // this angle was given to us by box2d's calculations
+      ctx.translate(-(this.x) * this.scale, -(this.y) * this.scale);
+      ctx.fillStyle = this.color;
+      ctx.drawImage(
+        nyanImg,
+        (this.x-this.halfWidth) * this.scale - 10, //lets offset it a little to the left
+        (this.y-this.halfHeight) * this.scale
+      );
+      ctx.restore();
+    }
+  });
+  game.box.addBody(nyan);
+  game.entities[nyan.id] = nyan;
+
+  geomId++;
+  yarn = new Circle({
+    id: 'yarn',
+    x: 600,
+    y: 390,
+    radius: 30,
+    staticBody: false,
+    density: 0.5,  // al little lighter
+    restitution: 0.8, // a little bouncier
+    draw: function(ctx){  //we also want to render the yarn with an image
+      ctx.save();
+      ctx.translate(this.x * this.scale, this.y * this.scale);
+      ctx.rotate(this.angle);
+      ctx.translate(-(this.x) * this.scale, -(this.y) * this.scale);
+      ctx.fillStyle = this.color;
+      ctx.drawImage(
+        yarnImg,
+        (this.x-this.radius) * this.scale,
+        (this.y-this.radius) * this.scale
+      );
+      ctx.restore();
+    }
+  });
+  game.box.addBody(yarn);
+  game.entities[yarn.id] = yarn;
+
 
   //if you want to take a look at the game object in dev tools
   console.log(game);
