6988a5556e23c43aecd4d023042c63a922808ad0
Blaine Bublitz <blaine@iceddev.com>
2013-02-18 13:42:05 -0700

    add docs to HTML5Audio and WebAudio

--- src/sounds/HTML5Audio.js
+++ src/sounds/HTML5Audio.js
@@ -1,3 +1,9 @@
+/**
+ * A Sound object that abstracts HTML5 Audio into a generic API that can also be used with WebAudio
+ * @name HTML5Audio
+ * @constructor HTML5Audio
+ */
+
 define([
   'dcl',
   'dojo/on',
@@ -7,16 +13,48 @@ define([
   'use strict';
 
   return dcl(null, {
+    /**
+     * The declared class - used for debugging in dcl
+     * @type {String}
+     * @memberOf HTML5Audio#
+     * @default
+     */
     declaredClass: 'frozen/sounds/HTML5Audio',
+    /**
+     * The name of the Sound object - typically the filename
+     * @type {String}
+     * @memberOf HTML5Audio#
+     * @default
+     */
     name: null,
+    /**
+     * Signals if the Sound object has completed loading
+     * @type {Boolean}
+     * @memberOf HTML5Audio#
+     * @default
+     */
     complete: false,
+    /**
+     * The initial Audio object - used to load the sound prior to playing
+     * @type {Audio}
+     * @memberOf HTML5Audio#
+     * @default
+     */
     audio: null,
+
     constructor: function(filename){
       this.audio = new Audio();
       if(typeof filename === 'string'){
         this.load(filename);
       }
     },
+
+    /**
+     * Load the sound by filename
+     * @function
+     * @memberOf HTML5Audio#
+     * @param  {String} filename The filename of the file to load
+     */
     load: function(filename){
       this.name = filename;
       this.audio.pause();
@@ -26,12 +64,27 @@ define([
         this.complete = true;
       }));
     },
+
+    /**
+     * Loop the sound at a certain volume
+     * @function
+     * @memberOf HTML5Audio#
+     * @param  {Number} volume Value of volume - between 0 and 1
+     */
     loop: function(volume){
       var audio = this._initAudio(volume, true);
       on(audio, 'loadeddata', function(e){
         audio.play();
       });
     },
+
+    /**
+     * Play the sound at a certain volume and start time
+     * @function
+     * @memberOf HTML5Audio#
+     * @param  {Number} volume    Value of volume - between 0 and 1
+     * @param  {Number} startTime Value of milliseconds into the track to start
+     */
     play: function(volume, startTime){
       startTime = startTime || 0;
 
@@ -41,6 +94,16 @@ define([
         audio.play();
       });
     },
+
+    /**
+     * Method used to construct Audio objects internally
+     * @function
+     * @memberOf HTML5Audio#
+     * @private
+     * @param  {Number} volume Value of volume - between 0 and 1
+     * @param  {Boolean} loop Whether or not to loop audio
+     * @return {Audio} Audio object that was constructed
+     */
     _initAudio: function(volume, loop){
       loop = typeof loop === 'boolean' ? loop : false;
 
--- src/sounds/WebAudio.js
+++ src/sounds/WebAudio.js
@@ -1,3 +1,9 @@
+/**
+ * A Sound object that abstracts WebAudio into a generic API that can also be used with HTML5 Audio
+ * @name WebAudio
+ * @constructor WebAudio
+ */
+
 define([
   'dcl',
   'dojo/on',
@@ -14,16 +20,54 @@ define([
   }
 
   return dcl(null, {
+    /**
+     * The declared class - used for debugging in dcl
+     * @type {String}
+     * @memberOf WebAudio#
+     * @default
+     */
     declaredClass: 'frozen/sounds/WebAudio',
+    /**
+     * The name of the Sound object - typically the filename
+     * @type {String}
+     * @memberOf WebAudio#
+     * @default
+     */
     name: null,
+    /**
+     * Signals if the Sound object has completed loading
+     * @type {Boolean}
+     * @memberOf WebAudio#
+     * @default
+     */
     complete: false,
+    /**
+     * The WebAudio AudioContext - used to perform operations on a sound
+     * @type {AudioContext}
+     * @memberOf WebAudio#
+     * @default
+     */
     audioContext: audioContext,
+    /**
+     * The sound buffer
+     * @type {Buffer}
+     * @memberOf WebAudio#
+     * @default
+     */
     buffer: null,
+
     constructor: function(filename){
       if(typeof filename === 'string'){
         this.load(filename);
       }
     },
+
+    /**
+     * Load the sound by filename
+     * @function
+     * @memberOf WebAudio#
+     * @param  {String} filename The filename of the file to load
+     */
     load: function(filename){
       this.name = filename;
 
@@ -48,16 +92,41 @@ define([
       on(request, 'load', decodeAudioData);
       request.send();
     },
+
+    /**
+     * Loop the sound at a certain volume
+     * @function
+     * @memberOf WebAudio#
+     * @param  {Number} volume Value of volume - between 0 and 1
+     */
     loop: function(volume){
       var audio = this._initAudio(volume, true);
       audio.noteOn(0);
     },
+
+    /**
+     * Play the sound at a certain volume and start time
+     * @function
+     * @memberOf WebAudio#
+     * @param  {Number} volume    Value of volume - between 0 and 1
+     * @param  {Number} startTime Value of milliseconds into the track to start
+     */
     play: function(volume, startTime){
       startTime = startTime || 0;
 
       var audio = this._initAudio(volume, false);
       audio.noteOn(startTime);
     },
+
+    /**
+     * Method used to construct Audio objects internally
+     * @function
+     * @memberOf WebAudio#
+     * @private
+     * @param  {Number} volume Value of volume - between 0 and 1
+     * @param  {Boolean} loop Whether or not to loop audio
+     * @return {Audio} Audio object that was constructed
+     */
     _initAudio: function(volume, loop){
       loop = typeof loop === 'boolean' ? loop : false;
 
