f87f8a1856b2a9a1850862dc9a6c5533541da501
Blaine Bublitz <blaine@iceddev.com>
2013-06-30 23:56:02 -0700

    only set the touchstart handler if we have webaudio

--- src/sounds/WebAudio.js
+++ src/sounds/WebAudio.js
@@ -24,16 +24,16 @@ define([
   var audioContext = null;
   if(has('WebAudio')){
     audioContext = new window.AudioContext();
-  }
 
-  if(has('shittySound')){
-    // Similar strategy to https://github.com/CreateJS/SoundJS
-    on.once(document, 'touchstart', function(){
-      var source = audioContext.createBufferSource();
-      source.buffer = audioContext.createBuffer(1, 1, 22050);
-      source.connect(audioContext.destination);
-      source.noteOn(0);
-    });
+    if(has('shittySound')){
+      // Similar strategy to https://github.com/CreateJS/SoundJS
+      on.once(document, 'touchstart', function(){
+        var source = audioContext.createBufferSource();
+        source.buffer = audioContext.createBuffer(1, 1, 22050);
+        source.connect(audioContext.destination);
+        source.noteOn(0);
+      });
+    }
   }
 
   return dcl(Sound, {
